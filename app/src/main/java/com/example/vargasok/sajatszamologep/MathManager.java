f90b9c657cfdf418d32c211d2e566c2fc2e7fdc0
heniboj <heniboj@gmail.com>
2018-11-27 22:45:10 +0100

    csak a BIN > DEC nem működik már csak

--- app/src/main/java/com/example/vargasok/sajatszamologep/MathManager.java
+++ app/src/main/java/com/example/vargasok/sajatszamologep/MathManager.java
@@ -2,36 +2,24 @@ package com.example.vargasok.sajatszamologep;
 
 import android.widget.Toast;
 
-
-
 public class MathManager {
 
     MainActivity main;
-
 
     public double DoMath(Operations op) {
 
         Operations prevOp = main.getPrevOp();
         double prevResult = main.getPrevResult();
-        szamrendszerek szamrendszer=main.getNumsys();
-        double ered;
-
-
-
 
         double input = main.Input();
         if (prevOp == null) {
             main.setPrevOp(op);
             if (!main.getJustOutputted())
                 return(input);
-           else
+            else
                 return(prevResult);
         }
         main.setPrevOp(op);
-
-
-        switch(szamrendszer){
-            case decimal:
         switch (prevOp) {
             case Addition:
                 return(prevResult + input);
@@ -49,22 +37,6 @@ public class MathManager {
                     return 0;
                 }
                 return(prevResult / input);
-        }break;
-
-            case binary:
-
-                prevResult=frombinary(prevResult);
-                input=frombinary(input);
-                switch(prevOp){
-
-                    case Addition:
-
-                    ered =prevResult+input;
-                        return(tobinary((int)ered));
-
-                        } break;
-
-
         }
 
         return prevResult;
@@ -73,31 +45,4 @@ public class MathManager {
     public MathManager(MainActivity _main) {
         main = _main;
     }
-
-
-
-    public double tobinary(int convertible){
-
-        String szam="";
-        while (convertible>0){szam=szam+convertible%2;convertible=convertible/2;}
-
-        if( szam.isEmpty()==true){  Toast.makeText(main, "Kérem adjon meg megfelelő értéket.", Toast.LENGTH_SHORT).show(); return(0);
-        }
-        else{
-            return(Double.parseDouble(new StringBuilder(szam).reverse().toString()));}
-    }
-
-
-    public double frombinary(double convertible){
-
-        char[] convertibleR = Double.toString(convertible).toCharArray();
-        int i =convertibleR.length;
-        while (i>0){if(convertibleR[i-1]=='1'){convertible=convertible+Math.pow(2,convertibleR.length-i);}i--;}
-
-        return (convertible);
-
-
-
-    }
-
 }
